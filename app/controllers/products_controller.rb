2a5c6c5cc0610c780447f13a152320e0b9355ab5
BJelenjev <boacon1@yahoo.com>
2017-07-14 00:31:53 +0200

    cart built and associated

--- app/controllers/products_controller.rb
+++ app/controllers/products_controller.rb
@@ -2,13 +2,13 @@ class ProductsController < ApplicationController
 
   def show
     @product = Product.find(params[:id])
-    @pictures = @product.all_pictures
-
+    @pictures = @product.all_photos
+    @customer = User.new
   end
 
   def index
     @products=Product.all
-
+    @customer = User.new
   end
 
 end
---
+++ spec/factories/carts.rb
@@ -0,0 +1,8 @@
+FactoryGirl.define do
+  factory :cart do
+    user nil
+    product nil
+    amount ""
+    ckecked_out ""
+  end
+end
